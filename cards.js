0cc685d489769152d721275b01e26c65ce4e2ca9
ellisthomas <ellisthomas80@gmail.com>
2017-03-21 23:01:48 -0500

    readmeeeeee

--- cards.js
+++ cards.js
@@ -7,17 +7,16 @@ function writeToDom() {
 	counter += 1
 	var createNewCard = "";
 	createNewCard += `<div class="cardBox" id="newBox-${counter}">`
-	createNewCard += `<h1> ${userInput.value} </h1>`;
+	createNewCard += `<h3> ${userInput.value} </h3>`;
 	createNewCard += `<img src="http://sites.muscogee.k12.ga.us/codingroboticsacademy/wp-content/uploads/sites/68/2016/05/coding-reading.jpg">`
 	createNewCard += `<button class="deleteButton">Delete</button>`
 	createNewCard += `</div>`
 	cardMakerDiv.innerHTML += createNewCard;
-	userText.value = "";
+	userInput.value = "";
 
 }
 
 createBtn.addEventListener("click", writeToDom);
-deleteButton.addEventListener("mouseenter", writeToDom);
 
 document.body.addEventListener("click",function(event){
 
